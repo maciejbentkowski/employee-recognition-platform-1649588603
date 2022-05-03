80a8884a3bc24dad4f3d419770d47f1ff9e79f34
Maciej Bentkowski <bentkowskimaciej@gmail.com>
2022-05-03 19:44:11 +0200

    Refactor kudo and admin system specs

--- spec/system/admin_system_spec.rb
+++ spec/system/admin_system_spec.rb
@@ -1,33 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe 'Admin', :type => :system do
+
     before do
       driven_by(:rack_test)
-
     end
 
+    let(:admin) { create(:admin) }
+    let(:admin2) { create(:admin) }
+    let(:admin3) { create(:admin) }
+    let(:employee1) { create(:employee) }
+    let(:employee2) { create(:employee) }
+    let(:employee3) { create(:employee) }
+    let!(:kudo1) { create(:kudo, giver_id: employee1.id, receiver_id: employee2.id) }
+    let!(:kudo2) { create(:kudo, giver_id: employee2.id, receiver_id: employee3.id) }
+    let!(:kudo3) { create(:kudo, giver_id: employee3.id, receiver_id: employee1.id) }
+
     it 'signs admin in' do
-      admin = build(:admin)
-      visit '/admins'
-      fill_in 'Email', with: admin.email
-      fill_in 'Password', with: admin.password
-      click_button 'commit'
-      expect(page).to have_content 'Signed in successfully.'
+      admin_sign_in_and_expect_succesfully
     end
 
     it 'listing all kudos' do
-      employee1 = create(:employee)
-      employee2 = create(:employee)
-      employee3 = create(:employee)
-      admin = build(:admin)
-      kudo1 = create(:kudo, giver_id: employee1.id, receiver_id: employee2.id)
-      kudo2 = create(:kudo, giver_id: employee2.id, receiver_id: employee3.id)
-      kudo3 = create(:kudo, giver_id: employee3.id, receiver_id: employee1.id)
-      visit '/admins'
-      fill_in 'Email', with: admin.email
-      fill_in 'Password', with: admin.password
-      click_button 'commit'
-      expect(page).to have_content "DASHBOARD"
+      admin_sign_in_and_expect_succesfully
       visit '/admins/kudos'
       expect(page).to have_content kudo1.title
       expect(page).to have_content kudo1.content
@@ -38,18 +32,7 @@ RSpec.describe 'Admin', :type => :system do
     end
 
     it 'deleting Kudos' do
-      employee1 = create(:employee)
-      employee2 = create(:employee)
-      employee3 = create(:employee)
-      admin = create(:admin)
-      kudo1 = create(:kudo, giver_id: employee1.id, receiver_id: employee2.id)
-      kudo2 = create(:kudo, giver_id: employee2.id, receiver_id: employee3.id)
-      kudo3 = create(:kudo, giver_id: employee3.id, receiver_id: employee1.id)
-      visit '/admins'
-      fill_in 'Email', with: admin.email
-      fill_in 'Password', with: admin.password
-      click_button 'commit'
-      expect(page).to have_content "DASHBOARD"
+      admin_sign_in_and_expect_succesfully
       visit '/admins/kudos'
       expect(page).to have_content kudo1.title
       expect(page).to have_content kudo1.content
@@ -58,6 +41,14 @@ RSpec.describe 'Admin', :type => :system do
       expect(page).to have_content kudo3.title
       expect(page).to have_content kudo3.content
       expect { (click_on "Destroy", match: :first) }.to change(Kudo, :count).by(-1)
+    end
+
+    def admin_sign_in_and_expect_succesfully
+      visit '/admins'
+      fill_in 'Email', with: admin.email
+      fill_in 'Password', with: admin.password
+      click_button 'commit'
+      expect(page).to have_content "Signed in successfully."
     end
 
   end
