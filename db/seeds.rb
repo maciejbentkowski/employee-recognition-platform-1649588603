3833aa460e65b686b0fc1819f73db8953ecfe5e6
Maciej Bentkowski <bentkowskimaciej@gmail.com>
2022-05-06 23:13:44 +0200

    Deleted views after revert
    -pushed schemas with admin devise model and number of available kudos that would be handy in future
    -added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,16 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
+employees = Array.new
 
 5.times do |e|
 employee = Employee.create!(email:"employee#{e+1}@sample.com", password:"password#{e+1}")
-
 puts employee.email
+employees[e] = employee
 end
-rand(1..20).times do |k|
-Kudo.create!(title: "sample title number #{k}", content: "sample content number #{k}", giver_id: rand(1..5), receiver_id: rand(1..5))
+employees.each do |e|
+    rand(1..10).times do |k|
+        Kudo.create!(title: "sample title number #{k}", content: "sample content number #{k}", giver_id: e.id, receiver_id: rand(1..5))
+    end
 
 end
